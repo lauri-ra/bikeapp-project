ded4b68f3951abc83600e036fb486e2561c5c774
lauri-ra <lrautiala+1@gmail.com>
2023-05-22 02:10:00 +0300

    Initial home page implementation

--- bikeapp/app/page.tsx
+++ bikeapp/app/page.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<div>bikeapp</div>
+		<main className='flex h-screen items-center justify-center'>
+			<div>
+				<div className='mb-10 flex justify-center text-6xl tracking-wide'>bikeapp</div>
+				<div className='flex justify-center text-lg'>
+					<Link href='/journeys'>
+						<button className='mr-4 rounded-lg bg-sky-500 px-6 py-4 font-semibold text-white hover:bg-sky-400'>
+							Journeys
+						</button>
+					</Link>
+					<Link href='/stations'>
+						<button className='ml-4 rounded-lg bg-sky-500 px-6 py-4 font-semibold text-white hover:bg-sky-400'>
+							Stations
+						</button>
+					</Link>
+				</div>
+			</div>
 		</main>
 	);
 }
