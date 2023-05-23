c44ee4617ebaa8fdac071a8b2408f137e3f5fea2
lauri-ra <lrautiala+1@gmail.com>
2023-05-23 20:36:53 +0300

    Simple stations page implementation

---
+++ bikeapp/app/stations/page.tsx
@@ -0,0 +1,50 @@
+import { prisma } from '../../prisma/connect';
+import Link from 'next/link';
+
+export default async function Page({
+	searchParams,
+}: {
+	searchParams: { [key: string]: string | string[] | undefined };
+}) {
+	// These help with the conditial rendering of the nav buttons.
+	const stationCount = await prisma.stations.count();
+	const maxPage = Math.round(stationCount / 10);
+
+	// Make sure that the page is of type string. If it is, turn it to a number, else default to 1.
+	const page = typeof searchParams.page === 'string' ? +searchParams.page : 1;
+
+	// Take 10 elements from the db according to the current page location.
+	const stations = await prisma.stations.findMany({
+		take: 10,
+		skip: (page - 1) * 10,
+	});
+
+	return (
+		<div>
+			{stations.map((station) => (
+				<div key={station.station_id} className='my-2 border-2'>
+					<div>{station.station_id}</div>
+					<div>{station.station_name_fi}</div>
+				</div>
+			))}
+			<div className='flex items-center justify-center'>
+				{page > 1 && (
+					<Link
+						href={`/stations/?page=${page - 1}`}
+						className='ml-2 rounded-lg bg-sky-500 px-4 py-2 text-sm font-semibold text-white hover:bg-sky-600'
+					>
+						Prev
+					</Link>
+				)}
+				{page < maxPage && (
+					<Link
+						href={`/stations/?page=${page + 1}`}
+						className='ml-2 rounded-lg bg-sky-500 px-4 py-2 text-sm font-semibold text-white hover:bg-sky-600'
+					>
+						Next
+					</Link>
+				)}
+			</div>
+		</div>
+	);
+}
