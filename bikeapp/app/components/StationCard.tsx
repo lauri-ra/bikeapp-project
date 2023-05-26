0d01239797a5c9df503f22e7ae50e485ce50ef09
lauri-ra <lrautiala+1@gmail.com>
2023-05-26 21:10:13 +0300

    Added top returns to single station view

--- bikeapp/app/components/StationCard.tsx
+++ bikeapp/app/components/StationCard.tsx
@@ -1,13 +1,15 @@
-import { Station } from '@/types';
 import React from 'react';
+import Link from 'next/link';
+import { Journey, Station } from '@/types';
 
 interface StationCardProps {
 	station: Station | null;
 	departures: number;
 	returns: number;
+	topReturns: Journey[];
 }
 
-const StationCard: React.FC<StationCardProps> = ({ station, departures, returns }) => {
+const StationCard: React.FC<StationCardProps> = ({ station, departures, returns, topReturns }) => {
 	const city = station?.city_fi === ' ' ? 'city not specified' : station?.city_fi;
 
 	return (
@@ -21,6 +23,20 @@ const StationCard: React.FC<StationCardProps> = ({ station, departures, returns 
 			<div>Capacity of {station?.capacity} bikes</div>
 			<div>Returns {returns}</div>
 			<div>Departures {departures}</div>
+
+			<div className='my-5'>
+				<div className='font-semibold'>
+					Top return stations for journeys starting from this station
+				</div>
+				{topReturns.map((station) => (
+					<div key={station.return_station_id} className='my-2 border-2'>
+						<Link href={`/stations/${station.return_station_id}`}>
+							<div>{station.return_station_id}</div>
+							<div>{station.return_station_name}</div>
+						</Link>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
--- bikeapp/app/stations/[id]/page.tsx
+++ bikeapp/app/stations/[id]/page.tsx
@@ -1,12 +1,49 @@
 import { prisma } from '@/prisma/connect';
 import StationCard from '@/app/components/StationCard';
+import { Journey } from '@/types';
+
+// TODO: Move this to api routes??
+async function getTopReturns(stationId: number): Promise<Journey[]> {
+	// TODO: Check if aggregation like this works with prisma
+	// const topReturns = await prisma.journeys.groupBy({
+	// 	by: ['return_station_id'],
+	// 	where: { departure_station_id: stationId },
+	// 	orderBy: {
+	// 		_count: {
+	// 			departure_station_id: 'desc',
+	// 		},
+	// 	},
+	// 	take: 5,
+	// });
+
+	const topReturns: Journey[] = await prisma.$queryRaw`
+		SELECT return_station_id, return_station_name
+		FROM journeys
+		WHERE departure_station_id = ${stationId}
+		GROUP BY return_station_id, return_station_name
+		ORDER BY COUNT(*) DESC
+		LIMIT 5;
+	`;
+
+	return topReturns;
+}
 
 export default async function Page({ params }: { params: { id: string } }) {
 	const stationId = parseInt(params.id);
 
 	const station = await prisma.stations.findUnique({ where: { station_id: stationId } });
+
 	const departures = await prisma.journeys.count({ where: { departure_station_id: stationId } });
 	const returns = await prisma.journeys.count({ where: { return_station_id: stationId } });
 
-	return <StationCard station={station} departures={departures} returns={returns} />;
+	const topReturns: Journey[] = await getTopReturns(stationId);
+
+	return (
+		<StationCard
+			station={station}
+			departures={departures}
+			returns={returns}
+			topReturns={topReturns}
+		/>
+	);
 }
